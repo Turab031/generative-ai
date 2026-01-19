efd7715930a8b06be264ff58190a7926fc54c218
Turab031 <147339712+Turab031@users.noreply.github.com>
2026-01-19 19:18:34 +0530

    updated

--- cot.py
+++ cot.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from groq import Groq
+import json
 
 load_dotenv()
 
@@ -33,6 +34,7 @@ SYSTEM_PROMPT = """
  PLAN:{"steps":"PLAN":"content":"Great,we have solved and left with 3.5 as ans "}
  OUTPUT:{"steps":"OUTPUT":"content":"3.5 "}
  
+ 
   
    
  
@@ -40,14 +42,61 @@ SYSTEM_PROMPT = """
  
 
 """
+
+message_history=[
+    {"role":"system","content":SYSTEM_PROMPT},
+]
+user_query=input("👉🏻")
+message_history.append({"role":"user","content":user_query})
+while True:
+    response = client.chat.completions.create(
+    
+        model="llama-3.1-8b-instant",
+        response_format={"type":"json_object"},
+        messages=message_history
+    )
+    
+    raw_result = (response.choices[0].message.content)
+    message_history.append({"role":"assistant","content":raw_result}),
+    parsed_result = json.loads(raw_result)
+    if parsed_result.get("step")=="START":
+        print("🔥",parsed_result.get("content"))
+        continue
+    
+    if parsed_result.get("step")=="PLAN":
+        print("🧠",parsed_result.get("content"))
+        
+
 response = client.chat.completions.create(
     
     model="llama-3.1-8b-instant",
     response_format={"type":"json_object"},
     messages=[
         {"role":"system","content":SYSTEM_PROMPT},
-        {"role": "user", "content":"Hey,write a code to add n numbers in js" }
-    ]
+        {"role": "user", "content":"Hey,write a code to add n numbers in js" },
+        {"role":"assistant","content":json.dumps({
+            "step":"PLAN",
+            "content":"We need to write a JavaScript function to add n numbers"
+        })},
+         {"role":"assistant","content":json.dumps({
+            "step":"PLAN",
+            "content": "The function will take an array of numbers as input, so the function declaration will be like addNumbers(arr) or simply add(arr)"
+        })},
+         
+        {"role":"assistant","content":json.dumps({
+            "step":"PLAN",
+            "content": "We will use the reduce() method of the array to add all the numbers in it and return the result"
+        })},
+        
+        {"role":"assistant","content":json.dumps({
+            "step":"PLAN",
+            "content": "Here's a sample JavaScript function: \nfunction add(arr) { \nreturn arr.reduce((total, number) => { return total + number; }); \n}"
+        })},
+         
+         
+         
+        
+    ] 
 )
 
 print(response.choices[0].message.content)
